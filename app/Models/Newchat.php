1906b9320cc55b786aff82933d196d78c04df80b
Abdelrahman Hassan <152273218+Abdelrahman5884@users.noreply.github.com>
2025-09-30 18:53:42 +0300

    Update Newchat.php

--- app/Models/Newchat.php
+++ app/Models/Newchat.php
@@ -6,7 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Newchat extends Model
 {
+    protected $table = 'newchats';
+    protected $fillable = ['title', 'user_id'];
 
+    public function chats()
+    {
+        return $this->hasMany(Chat::class);
+    }
     public function user()
     {
         return $this->belongsTo(User::class);
