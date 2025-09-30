4a1e910331470027c8c1ebf09d361f2412b1ec14
Abdelrahman Hassan <152273218+Abdelrahman5884@users.noreply.github.com>
2025-09-30 18:53:22 +0300

    Update Chat.php

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Chat extends Model
 {
 
+     protected $fillable = ['name', 'user_id', 'prompt', 'response', 'newchat_id'];
+
+    public function newchat()
+    {
+        return $this->belongsTo(NewChat::class);
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
